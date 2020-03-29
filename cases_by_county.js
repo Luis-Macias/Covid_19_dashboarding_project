809f53fbc64582628102af0e8d28a2037b8bbe70
Luis Macias <david.macias@berkeley.edu>
2020-03-28 17:53:34 -0700

    added filter to Promise chain to first work on a single day

--- cases_by_county.js
+++ cases_by_county.js
@@ -1,6 +1,6 @@
 var h1 = d3.select('body')
     .append('h1') // making a new div svg element 
-    .text("U.S COVID-19 Positive Cases by State")
+    .text("U.S COVID-19 Positive Cases by County")
 
 const margin = {
     top: 30,
@@ -33,4 +33,39 @@ let posmap = d3.map();
 
 let totalmap = d3.map();
 let percapmap = d3.map();
-var url = "https://covidtracking.com/api/states.csv"
+
+// url to the dataset that NYTimes is currently using for their cornonavirus dashboard 
+// var county_cases = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+// var county_totals = {}
+// let county_total_pop = d3.csv('./data/state_total_pop.csv', function(d) {
+//         state_totals[d.abrev] = {
+//             state: d.states,
+//             population: +d.total
+//         }
+//     }
+
+// )
+let time_parse = d3.timeParse("%Y-%m-%d")
+
+let county_data = d3.csv('data/us-counties.csv', function(d) {
+    // console.log(d)
+    return {
+        date: time_parse(d.date),
+        county: d.county,
+        state: d.state,
+        fips: d.fips,
+        cases: +d.cases,
+        deaths: +d.deaths
+    }
+}).then(function(data) {
+    return data.filter(function(d, i) {
+        return d.date.getTime() === time_parse('2020-03-27').getTime();
+    });
+})
+
+
+// let temp1 = county_data.filter(d => d.date < time_parse('2020-03-27'))
+// let temp2 = d3.csv('data/us-counties.csv')
+// .then(function(data){
+//     console.log(data)
+// })
