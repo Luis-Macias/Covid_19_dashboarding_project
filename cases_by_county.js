b87496ced3faf93ab68290602104fddb9c4e4e75
Luis Macias <david.macias@berkeley.edu>
2020-04-04 14:29:30 -0700

    added a container for the scrubber and a tooltip for on mouseover function

--- cases_by_county.js
+++ cases_by_county.js
@@ -3,14 +3,7 @@
 console.log("hello world")
 // import { scrubber } from './modules/scrubber.js';
 
-function getLocation(d) {
-    let location = countyMap.get(d.fips);
-    if (!location && d.county === "New York City")
-        location = countyMap.get("36061");
-    if (!location) location = stateMap.get(d.state);
-    if (!location) console.warn("No location found for: " + JSON.stringify(d));
-    return location;
-}
+
 // defining our margins 
 const margin = {
     top: 30,
@@ -32,7 +25,8 @@ var h1 = d3.select('body')
 var projection = d3.geoAlbersUsa()
 // .scale([1100]) // scale things down so see entire US
 // .translate([width / 2, height / 2]) // translate to center of screen
-// creating a transition 
+
+// making a transition function 
 var t = d3.transition().duration(250).ease(d3.easeLinear)
 
 // Define path generator
@@ -40,51 +34,65 @@ var path = d3.geoPath()
     .projection(projection);
 // .scale(1000);
 
-// creating our svg element that will we draw on 
+// creating our svg view that will we draw on 
 var svg = d3.select("body")
     .append("svg")
     .attr("width", width)
-    .attr("height", height)
+    .attr("height", height);
+
+var tooltip = d3.select('body')
+    .append('div');
+
+var form_container = d3.select('body')
+    .append('div');
 
 // creating our radius scale 
 var radius_pos = d3.scaleSqrt().range([0, 50]);
 
-// parsing the string of dates to convert to Date objects
-let time_parse = d3.timeParse("%Y-%m-%d")
+// creating a function to parse the string of dates to convert to Date objects
+let time_parse = d3.timeParse("%Y-%m-%d");
 
-// loading in TOPOJSON data
-let county_topo = d3.json('data/counties-10m.json')
+// loading in TOPOJSON data data taken from https://github.com/topojson/us-atlas
+let county_topo = d3.json('data/counties-10m.json');
 
-
+// making a Promise that holds a Map of 50 U.S states + D.C to cartographic boundaries 
 let state_map = county_topo.then(us => new Map(
-    topojson.feature(us, us.objects.states).features.filter(function (d, i) {
+    topojson.feature(us, us.objects.states).features.filter(function(d, i) {
         // ids above 56 are several US territories like Guam , and Puetro Rico
-        return !(d.id > 56)
-    }).map(d => [d.properties.name, d])))
+        return !(d.id > 56);
+    }).map(d => [d.properties.name, d])));
 
 
-// creating a map of counties
+// like the state map except for  U.S counties 
 let county_map = county_topo.then(us => new Map(
-    topojson.feature(us, us.objects.counties).features.map(d => [d.id, d])))
+    topojson.feature(us, us.objects.counties).features.map(d => [d.id, d])));
 
 
-// loading our covid data and parsing them 
-let get_clean_data = async() => {
+// loading our covid data that we preprocessed before
+// note we can also use the url https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv to load in the data
+let get_clean_data = async () => {
+    // we await for the state_map to complete as we use it for filtering u.s terroritories
     var mappings = await state_map;
-    var data = d3.csv('./data/us_counties.csv', function (d) {
 
+    var data = d3.csv('./data/us_counties.csv', function(d) {
+        // our preprocssed data should have include NYC's fips code but we leave this in in case of data being obtained from the url
         if (d['county'] === 'New York City') {
             d['fips'] = '36061'
         }
-        return d
-    }).then(function (data) {
-        // filtering the above data to be a specific data for practice
+        return d;
+    }).then(function(data) {
+        // reshaping our data into an array of nested objects 
+        // keys are dates and values contain county names,  state name,  fips code, # of positive cases , # of deaths 
+        // 
         return d3.nest()
-            .key(function (d) {
+            .key(function(d) {
                 return d.date
-            }).sortKeys(d3.ascending)
+            })
+            .sortKeys(d3.ascending) // sort keys so march first is index 0 and our recent date is index data.length -1 
             .entries(
-                data.filter(d => time_parse(d.date).getTime() >= time_parse('2020-03-01').getTime())
+                data.filter(
+                    d => time_parse(d.date).getTime() >= time_parse('2020-03-01').getTime()
+                )
                 .filter(d => mappings.has(d.state))
             )
 
@@ -92,97 +100,151 @@ let get_clean_data = async() => {
     })
     return data
 }
-let covid_by_county = get_clean_data();
 
+let covid_by_county = get_clean_data();
 
 // max cases is a promise that when resolved should update the domain of our radius scale
 let max_cases = covid_by_county.then(d => d[d.length - 1])
     .then(d => d3.max(d.values.map(d => +d['cases'])))
-    .then(d => radius_pos.domain([0, d]))
+    .then(d => radius_pos.domain([0, d]));
 
 
 function chart() {
+    // the chart relies on the below 4 promises to be created hence the need for Promise.all([])
     Promise.all([covid_by_county, max_cases, county_topo, county_map, state_map])
-        .then(function (data) {
+        .then(function(data) {
             // console log our promises for debugging/ sanity checking
-            console.log(data)
-            var a = data[0]
+            // console.log(data)
+            var covid_cases = data[0]
+
             // creating nation borders !
             svg.append('path')
-                // .data(data[2],function(data){return data[2].filter(function(d,i){ return topojson.feature(d, d.objects.states).features.id.has(posmap.keys())} )})
                 .datum(topojson.feature(data[2], data[2].objects.nation))
-                // .enter()
                 .attr('d', path)
                 .attr('class', 'nation')
                 .style('fill', "#ccc")
 
             // creating state borders
+            // mesh is used to only draw interal state borders  
             svg.append('path')
-                .datum(topojson.mesh(data[2], data[2].objects.states, function (a, b) {
+                .datum(topojson.mesh(data[2], data[2].objects.states, function(a, b) {
                     return a !== b;
                 }))
-                // .enter()
                 .attr("class", "border ")
                 .attr('d', path)
 
 
-            // creating county centroids
+            // creates county centroids
             var circles = svg.append("g")
                 .attr("class", "bubble")
                 .selectAll("circle")
-                .data(a[a.length - 1].values.sort(function (a, b) {
+                .data(covid_cases[0].values.sort(function(a, b) {
                     return +b.cases - +a.cases
                 }))
                 .enter()
                 .append("circle")
-            // 
-            console.log(circles)
+            // used for debugging 
+            // console.log(circles)
 
-            circles.attr("transform", function (d) {
+            circles.attr("transform", function(d) {
+                //  location tries to get geographic info based on data's county fips code if not found attempt to get the state geographic info
                 var location = data[3].get(d.fips) ? data[3].get(d.fips) : data[4].get(d.state)
-
                 if (!location) {
+                    // included for debugging without processing errors usually show up for data pertaining to u.s  territories
                     console.warn("No location found for: " + JSON.stringify(d))
                 }
+
                 return "translate(" + path.centroid(location) + ")";
             })
             // .attr("r", function(d) {
             //     return radius_pos(+d.cases)
             // })
 
-            // creat state labels
+            // creating state labels based on where state centroid positions would be 
             svg.append('g')
                 .attr('class', 'State Legend')
                 .selectAll('text')
-                .data(topojson.feature(data[2], data[2].objects.states).features.filter(function (d, i) {
+                .data(topojson.feature(data[2], data[2].objects.states).features.filter(function(d, i) {
                     // ids above 56 are several US territories like Guam , and Puetro Rico
+                    // return data should only feature the 50 u.s states + D.C
                     return !(d.id > 56)
                 }))
                 .enter()
                 .append('text')
-                .attr('dx', function (d) {
+                .attr('dx', function(d) {
                     return path.centroid(d)[0] - 10
                 })
-                .attr('dy', function (d) {
+                .attr('dy', function(d) {
                     // console.log(d)
                     return path.centroid(d)[1]
                 })
-                .text(function (d) {
+                .text(function(d) {
                     return d.properties.name
                 })
                 .style('align', 'left')
                 .style('vertical-align', 'middle')
-                .style('font-size', 10)
-        }).catch(function (error) {
+                .style('font-size', 10);
+        }).catch(function(error) {
             console.log(error)
         })
 }
 chart()
 
+// function that updates our county bubbles based on index of array of Dates [march 1 , ... recent date in data]
+async function update_chart(index) {
+// queueing up Promises need to actually update our circles
+let result = await Promise.all([covid_by_county, county_map, state_map])
 
-var tooltip = d3.select('body')
-    .append('div')
+// making a variable of the selection of circles 
+let circles = svg.select('g').selectAll('circle')
 
+// joining our updated data and sorting based on descening case count to draw biggger bubles first and smaller bubbles on top   
+result[0][index].values
+circles
+    .data(result[0][index].values.sort(function(a, b) {
+        return +b.cases - +a.cases
+    }), d => d.fips || d.county)
+    // utilizing .jion method to specify what occurs to data being enter updateded and exited
+    .join(
+        enter => enter.append("circle")
+        .attr("transform", function(d) {
+            var location = result[1].get(d.fips) ? result[1].get(d.fips) : result[2].get(d.state)
+            if (!location) {
+                console.warn("No location found for: " + JSON.stringify(d))
+            }
+            return "translate(" + path.centroid(location) + ")";
+        })
+        .attr("r", function(d) {
+            return radius_pos(+d.cases)
+        }),
+
+        update => update
+        .attr("transform", function(d) {
+            var location = result[1].get(d.fips) ? result[1].get(d.fips) : result[2].get(d.state)
+            if (!location) {
+                console.warn("No location found for: " + JSON.stringify(d))
+            }
+            return "translate(" + path.centroid(location) + ")";
+        })
+        .attr("r", function(d) {
+            return radius_pos(+d.cases)
+        }),
+
+        exit => exit.attr("r", d => radius_pos(0)).call(
+            exit => exit.transition(t)
+            .attr("r", d => radius_pos(0)).remove()
+        )
+
+    )
+
+};
+
+
+/*
+scrubber function source code author: Mike Bostock, origin : https://observablehq.com/@mbostock/scrubber
+scrubber function was modfied to work outside of Observable notebook 
+scrubber returns a scrubber with Pause, Play , interactive control, allowing animations to be made from one start point to on end point 
+*/
 function scrubber(values, container, {
     format = value => value,
     delay = null,
@@ -253,57 +315,25 @@ function scrubber(values, container, {
     return form.o.value
 }
 
-async function update_chart(index) {
-
-    let result = await Promise.all([covid_by_county, county_map, state_map])
-    let circles = svg.select('g').selectAll('circle')
-
-    circles
-        .data(result[0][index].values.sort(function (a, b) {
-            return +b.cases - +a.cases
-        }))
-        .join(
-            enter => enter.append("circle")
-            .attr("transform", function (d) {
-                var location = result[1].get(d.fips) ? result[1].get(d.fips) : result[2].get(d.state)
-                if (!location) {
-                    console.warn("No location found for: " + JSON.stringify(d))
-                }
-                return "translate(" + path.centroid(location) + ")";
-            })
-            .attr("r", function (d) {
-                return radius_pos(+d.cases)
-            }),
-
-            update => update.attr("transform", function (d) {
-                var location = result[1].get(d.fips) ? result[1].get(d.fips) : result[2].get(d.state)
-                if (!location) {
-                    console.warn("No location found for: " + JSON.stringify(d))
-                }
-                return "translate(" + path.centroid(location) + ")";
-            })
-            .attr("r", function (d) {
-                return radius_pos(+d.cases)
-            }),
-
-            exit => exit.attr("r", d => radius_pos(0)).call(
-                exit => exit.transition()
-                .attr("r", d => radius_pos(0))
-            )
-
-        )
-};
-
 
 // update_chart(20)
 
 async function form_maker() {
     var dates = await covid_by_county.then(d => d.map(d => time_parse(d.key)))
-    scrubber(dates, tooltip, {
-        delay: 500,
+    scrubber(dates, form_container, {
+        delay: 750,
         autoplay: false,
         loop: false,
         format: d => d.toLocaleDateString()
     })
 }
 form_maker()
+
+
+// function measure(index){
+// var t0 = performance.now()
+
+// update_chart(index)   // <---- The function you're measuring time for 
+
+// var t1 = performance.now()
+// console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")}
